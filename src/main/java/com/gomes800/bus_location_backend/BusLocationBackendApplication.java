334a6ea06914be37871c0237bb08894c1066377f
barox <joaogomesleite800@gmail.com>
2025-01-24 08:47:36 -0300

    First commit

---
+++ src/main/java/com/gomes800/bus_location_backend/BusLocationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gomes800.bus_location_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusLocationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusLocationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomes800/bus_location_backend/BusLocationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomes800.bus_location_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusLocationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
