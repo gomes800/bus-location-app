3fa773a5c1699f8c6b6be195f055a583ba6a4418
barox <joaogomesleite800@gmail.com>
2025-01-24 13:15:26 -0300

    Filter recent data, changed memory allocation and other changes

--- src/main/java/com/gomes800/bus_location_backend/domain/BusLocation.java
+++ src/main/java/com/gomes800/bus_location_backend/domain/BusLocation.java
@@ -1,9 +1,7 @@
 package com.gomes800.bus_location_backend.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 @Getter
 @Setter
@@ -11,12 +9,19 @@ import lombok.Setter;
 @AllArgsConstructor
 public class BusLocation {
 
+    @JsonProperty("ordem")
     private String order;
     private String latitude;
     private String longitude;
+    @JsonProperty("datahora")
     private String dateTime;
+    @JsonProperty("velocidade")
     private String speed;
+    @JsonProperty("linha")
     private String line;
+    @JsonProperty("datahoraenvio")
     private String dateTimeShipping;
+    @JsonProperty("datahoraservidor")
     private String dateTimeServer;
+
 }
--- src/main/java/com/gomes800/bus_location_backend/services/BusService.java
+++ src/main/java/com/gomes800/bus_location_backend/services/BusService.java
@@ -4,13 +4,17 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gomes800.bus_location_backend.domain.BusLocation;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,16 +24,33 @@ public class BusService {
     private final ObjectMapper objectMapper;
 
     public BusService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("https://dados.mobilidade.rio").build();
+
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(100 * 1024 * 1024))
+                .build();
+        this.webClient = webClientBuilder
+                .baseUrl("https://dados.mobilidade.rio")
+                .exchangeStrategies(strategies)
+                .build();
         this.objectMapper = new ObjectMapper();
     }
 
     @Cacheable(value = "bus", key = "#line")
     public Mono<List<BusLocation>> getBusPerLine(String line) {
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime startDate = now.minusSeconds(100);
+        LocalDateTime endDate = now;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd+HH:mm:ss");
+        String startDateStr = startDate.format(formatter);
+        String endDateStr = endDate.format(formatter);
+
         return webClient.get()
-                .uri("/gps/sppo?dataInicial=AAAA-MM-DD+HH:MM:SS&dataFinal=AAAA-MM-DD+HH:MM:SS")
+                .uri("/gps/sppo?dataInicial={startDate}&dataFinal={endDate}", startDateStr, endDateStr)
                 .retrieve()
                 .bodyToMono(String.class)
+                .timeout(Duration.ofSeconds(30))
                 .map(data -> filterByLine(data, line));
     }
 
@@ -39,7 +60,18 @@ public class BusService {
 
             return busLocationList.stream()
                     .filter(bus -> line.equals(bus.getLine()))
-                    .collect(Collectors.toList());
+                    .collect(Collectors.groupingBy((BusLocation::getOrder)))
+                    .values() .stream()
+                    .map(group -> group.stream()
+                            .max((o1, o2) -> Long.compare(Long.parseLong(o1.getDateTime()), Long.parseLong(o2.getDateTime())))
+                                    .orElse(null))
+                            .filter(Objects::nonNull)
+                            .peek(bus -> {
+                                bus.setLatitude(bus.getLatitude().replace(",", "."));
+                                bus.setLongitude(bus.getLongitude().replace(",", "."));
+                            })
+                            .collect(Collectors.toList());
+                            
         } catch (IOException e) {
             throw new RuntimeException("Erro ao processar JSON", e);
         }
